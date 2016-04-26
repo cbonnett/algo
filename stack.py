ab49bc5c69536e1f335e6b7a4a165c1c33a8b387
christopher_bonnett <c.bonnett@gmail.com>
2016-04-26 23:09:44 +0200

    added queu and stack

---
+++ stack.py
@@ -0,0 +1,23 @@
+"""
+stack implemtented in python
+"""         
+
+class Stack:
+    def __init__(self):
+        self.items= []
+    
+    def is_empty(self):
+        return self.items == []
+        
+    def push(self, item):
+        return self.items.append(item)
+         
+    def pop(self):
+        return self.items.pop()
+        
+    def peek(self):
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+      
