c3ded6e625938114c38d820d117a51afaed810b8
christopher_bonnett <c.bonnett@gmail.com>
2016-05-20 16:24:56 +0200

    added hastable

--- search.py
+++ search.py
@@ -38,4 +38,74 @@ def rec_b_search(alist, item):
             return rec_b_search(alist[:midpoint], item)
         else:
             return rec_b_search(alist[midpoint +1:], item)
+            
+def string_hash(astring, table_size):
+    """
+    Postion weighted string hash
+    :type astring: str
+    :type table_size: int
+    :rtype int 
+    """
+    
+    sum = 0
+    for i, char in enumerate(list(astring)):
+        sum = sum + i*ord(char)
+    return sum % table_size
+    
+    
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
         
+    def put(self, key, value):
+        hashvalue = self.hash_function(key, self.size)
+        
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = value
+        else:
+            if self.slots[hasvalue] == key:
+               self.data[hashvalue] = value
+            else:
+                new_hashvalue =  rehash_function(key + 1, self.size)
+                while self.slots[new_hashvalue] !=None:
+                    new_hashvalue = self.rehash_function(new_hashvalue, self.size)
+                
+                if self.slots[new_hashvalue] == None:
+                    self.slots[new_hashvalue] = key
+                    self.data[new_hashvalue] = value
+                else:
+                    self.data[new_hashvalue] = value
+
+    def get(self, key):
+        lookvalue = self.hash_function(key, self.size)
+        
+        data = None
+        found = False
+        stop = False
+        startpos = lookvalue
+        
+        while (self.slots[startpos] !=None) and (not found) and (not stop):
+            if self.slots[startpos] == key:
+                data = self.data[startpos]
+                found = True
+        else:
+            startpos = self.rehash_function(startpos, self.size)
+            if startpos == lookvalue:
+                stop = True
+            
+        return data
+     
+    def hash_function(self, key, table_size):
+        return key % table_size
+        
+    def rehash_function(self, key, table_size):
+        return (key + 1) % table_size 
+    
+    def __setitem__(self, key, value):
+        return self.put(key, value)
+        
+    def __getitem__(self, key):
+        return self.get(key)
