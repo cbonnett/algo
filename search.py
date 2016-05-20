29d416640dfbb3d4391184ebbe75a9990f4c4275
christopher_bonnett <c.bonnett@gmail.com>
2016-05-20 13:28:14 +0200

    added recusrive binary search

--- search.py
+++ search.py
@@ -16,5 +16,26 @@ def b_search(alist, item):
         else :
             firstpoint = midpoint + 1
     return found
-                                    
+                        
+                        
+def rec_b_search(alist, item):
+    """"
+    Recursive Binary search to be used on sorted list
+    :type alist: list
+    :rtype: bool
+    """
+    if len(alist) == 0:
+        print 'false'
+        return False
+    else:
+        begin = 0
+        end = len(alist)-1
+        midpoint = (begin + end)//2
+        if alist[midpoint] == item:
+            print 'true'
+            return True
+        elif alist[midpoint] > item:
+            return rec_b_search(alist[:midpoint], item)
+        else:
+            return rec_b_search(alist[midpoint +1:], item)
         
